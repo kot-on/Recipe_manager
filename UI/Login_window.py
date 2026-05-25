9cde23cbea7b9d91ab08a00be9819e0b16d0b569
kot-on <efrem.egor35@gmail.com>
2026-05-25 11:29:19 +1000

    first UI commit

---
+++ UI/Login_window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+root = Tk()
+root.title("Login window")
+root.geometry("700x500")
+label = Label(text="Recipe manager",fg='#585B91',font=('Inter',50),bg='Black')
+label.pack()
+root.configure(bg='Black')
+root.mainloop()
