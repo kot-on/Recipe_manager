0163457ed70f1ba0a481818be7db1b4b930fd919
slava <mewkereter454@gmail.com>
2026-06-08 01:58:41 +1000

    создал ключ доступа для входа в бд

--- Database/db.py
+++ Database/db.py
@@ -1,17 +1,58 @@
 import sqlite3
 import os
+import getpass  
 
-# Путь к бд, чтобы не писать его везде
-# Семен тебе нужно будет работать с шифрованием этого канала подключения к бд
+# Путь к БД
 DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'bd.db')
 
-def get_connection():
-    return sqlite3.connect(DB_PATH)
 
-def show_3_recipes(User_id,limit=3):
+def get_connection():
+    password = os.getenv("DB_PASSWORD")
+
+    if not password:
+        raise RuntimeError("DB_PASSWORD не задана в переменных окружения")
+
+    user_input = getpass.getpass("Введите пароль для доступа к базе: ")
+
+    if user_input != password:
+        raise PermissionError("Неверный пароль. Доступ запрещён.")
+
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute(f"PRAGMA key='{password}';")
+    conn.execute("SELECT count(*) FROM sqlite_master;")
+
+    return conn
+
+
+def show_3_recipes(User_id, limit=3):
     conn = get_connection()
-    cursor=conn.cursor()
-    cursor.execute("Select title, description, rating, image_path FROM recipes where User_id = ? LIMIT ?",(User_id,limit))
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT title, description, rating, image_path
+        FROM Recipes
+        WHERE User_id = ?
+        LIMIT ?
+        """,
+        (User_id, limit)
+    )
+
     recipes = cursor.fetchall()
     conn.close()
+
     return recipes
+
+
+if __name__ == "__main__":
+    try:
+        conn = get_connection()
+        print("База успешно открыта!")
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        print(cursor.fetchall())
+
+        conn.close()
+    except PermissionError as e:
+        print(f"Ошибка доступа: {e}")
