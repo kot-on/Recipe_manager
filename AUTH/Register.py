6228267313da62be4f7e825a5f859458ef0c5f41
pampers67jopa <sementurnak31193@gmail.com>
2026-06-15 04:30:03 +1000

    Update Register.py

--- AUTH/Register.py
+++ AUTH/Register.py
@@ -1,32 +1,76 @@
-import hashlib
+from tkinter import *
+from tkinter import messagebox
+import customtkinter
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) #тоже самое для видимости файлов
+from AUTH.Register import register_user
+from Main_menu import open_main_window #Переброс в главное меню
+from PIL import Image,ImageTk
+import re
+from AUTH.Login import login_user
+def open_register(root):
+    reg_window = Toplevel()
+    reg_window.title("Register window")
+    reg_window.geometry("700x500")
+    reg_window.configure(bg='Black')
+    image_path = "Assets/Logo.png"
+    pil_image = Image.open(image_path) 
+    img = ImageTk.PhotoImage(pil_image)
+    Image_label = Label(reg_window,image=img,bg='black')
+    Image_label.image = img
+    Image_label.pack(pady=(20,5))
+    
+    label = Label(reg_window, text="Регистрация", fg='#585B91', font=('Inter', 50), bg='Black')
+    label.pack(pady=(0,20))
 
-from Database.db import get_connection
-from Logger_logic import AuditLogger
+    entry_login = customtkinter.CTkEntry(reg_window,
+                                         placeholder_text="Логин",
+                                         width=212, height=49,
+                                         corner_radius=10, font=('Inter', 25)) #Ввод логина
+    entry_login.pack(pady=10)
 
-def register_user(username, password):
-    password_hash = hashlib.sha256(password.encode()).hexdigest()
+    entry_pass = customtkinter.CTkEntry(reg_window,
+                                        placeholder_text="Пароль",
+                                        width=212, height=49,
+                                        corner_radius=10, font=('Inter', 25),show='*') #Ввод пароля
+    entry_pass.pack(pady=10)
+
+    entry_pass2 = customtkinter.CTkEntry(reg_window,
+                                         placeholder_text="Повторите пароль",
+                                         width=212, height=49,
+                                         corner_radius=10, font=('Inter', 25),show='*') # Ввод пароля для сверка
+    entry_pass2.pack(pady=10)
+    def register(): #Регистрация с проверкой пароля
+        username = entry_login.get()
+        password1 = entry_pass.get()
+        password2 = entry_pass2.get()
+
+        if not re.match(r'^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};:\'",.<>?/\\|`~ ]+$', username):
+            messagebox.showerror("Ошибка", "Логин должен содержать только латинские символы")
+            return
     
-    conn = get_connection()
-    cursor = conn.cursor()
-    
-    # Проверка: существует ли уже такой пользователь
-    cursor.execute("SELECT id FROM users WHERE username = ?", (username,))
-    if cursor.fetchone():
-        conn.close()
-        return None
-    
-    cursor.execute(
-        "INSERT INTO users (username, password) VALUES (?, ?)",
-        (username, password_hash)
-    )
-    conn.commit()
-    user_id = cursor.lastrowid
-    
-    # Логируем регистрацию
-    AuditLogger.log(user_id, username, "REGISTER", entity_type="USER", entity_id=user_id, details=f"Регистрация пользователя {username}", status="SUCCESS")
-    
-    conn.close()
-    return user_id
+        if not re.match(r'^[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};:\'",.<>?/\\|`~ ]+$', password1):
+            messagebox.showerror("Ошибка", "Пароль должен содержать только латинские символы")
+            return
+
+        if len(password1) < 8:
+            messagebox.showerror("Ошибка", "Пароль должен содержать 8 и более символов")
+            return
+
+        if password1 == password2:
+                User_id = register_user(username, password1)  # сразу получаем id
+                reg_window.destroy()
+                root.destroy()  # закрываем окно логина тоже
+                open_main_window(User_id)
+        else:
+            messagebox.showerror("Ошибка", "Пароли не совпадают")
+        return 
+
+    btn_reg = customtkinter.CTkButton(reg_window,
+                                      text="Зарегистрироваться",
+                                      width=212, height=49,
+                                      fg_color='#63078E', font=('Inter', 25),
+                                      corner_radius=10,
+                                      command=register) #Кнопка реги
+    btn_reg.pack(pady=10)
