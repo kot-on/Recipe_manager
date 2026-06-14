52b51ea6bd499916dc6021bd64636934049bdc98
pampers67jopa <sementurnak31193@gmail.com>
2026-06-14 23:14:10 +1000

    Update db.py

--- Database/db.py
+++ Database/db.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-
+from Logger_logic import AuditLogger
 # Путь к бд, чтобы не писать его везде
 # Семен тебе нужно будет работать с шифрованием этого канала подключения к бд
 DB_PATH = os.path.join(os.path.dirname(__file__), '..', 'bd.db')
